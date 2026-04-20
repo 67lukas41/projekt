dab47b87ad5799e5768ad8a3c84069c4f7d5d20f
67lukas41 <lukolo@edu.umea.se>
2026-04-20 14:58:53 +0200

    .

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<main>
+    <h1>flizz</h1>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    h1 {
+        font-size: 4rem;
+        margin: 0;
+    }
+</style>
